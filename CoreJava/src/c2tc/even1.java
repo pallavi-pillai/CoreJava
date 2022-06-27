aabb8179081ba63392dcf47e113f0f49c20f3388
PALLAVI <PALLAVI@LAPTOP-CA37EMN7>
2022-06-27 12:01:44 +0530

    first 9 programs

---
+++ CoreJava/src/c2tc/even1.java
@@ -0,0 +1,16 @@
+package c2tc;
+import java.util.*;
+public class even1 {
+public static void main(String a[]) {
+	Scanner sc= new Scanner(System.in); 
+	int n=sc.nextInt();
+	if((n&1)!=1)
+	{
+		System.out.println("even");
+	}
+	else
+	{
+		System.out.println("odd");
+	}
+}
+}
---
+++ CoreJava/src/c2tc/hello.java
@@ -0,0 +1,9 @@
+package c2tc;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.print("Pallavi");
+		System.out.printf("100");
+		}
+}

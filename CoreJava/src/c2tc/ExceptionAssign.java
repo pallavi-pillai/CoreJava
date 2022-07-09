b5a4316bd0077a486a330fa88480c943ed38c6cd
pallavi-pillai <pallavipillai322@gmail.com>
2022-07-10 00:42:12 +0530

    Exception assignment

---
+++ CoreJava/src/c2tc/ExceptionAssign.java
@@ -0,0 +1,41 @@
+package c2tc;
+
+import java.util.Scanner;
+
+public class ExceptionAssign{
+	 
+		public static void main(String a[]) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=sc.nextInt();
+		
+		
+		try
+		{
+			validate(n,p);
+			int pow=1;
+			for (int i = 1; i <= p; i++)
+			{
+	         pow *= n;
+			}
+			System.out.println(pow);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+	private static void validate(int n,int p) throws Exception{
+		if(n<0 || p<0)
+		{
+			throw new java.lang.Exception("n or p should not be negative");
+		}
+		else if(n==0 || p==0)
+		{
+			throw new java.lang.Exception("n and p should not be zero");
+		}
+	}
+	}
+
+
+
